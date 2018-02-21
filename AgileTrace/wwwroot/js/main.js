c3acec1406b27b2343520f129e4eedf83edc3a36
kklldog <kklldog@qq.com>
2018-02-22 01:03:55 +0800

    add loadingbar

--- AgileTrace/wwwroot/js/main.js
+++ AgileTrace/wwwroot/js/main.js
@@ -1,30 +1,34 @@
-﻿var app = angular.module('app', ['ui.router', 'agile.bootstrap-pagebar', 'ngCookies','ngAnimate']);
+﻿var app = angular.module('app',
+    ['ui.router',
+        'angular-loading-bar',
+        'agile.bootstrap-pagebar',
+        'ngCookies', 'ngAnimate']);
 app.config(function ($stateProvider, $urlRouterProvider) {
-        var dashState = {
-            name: 'dash',
-            url: '/dash',
-            templateUrl: '/home/getview?viewName=dash',
-            controller: 'dashCtrl'
-        };
+    var dashState = {
+        name: 'dash',
+        url: '/dash',
+        templateUrl: '/home/getview?viewName=dash',
+        controller: 'dashCtrl'
+    };
 
-        var queryState = {
-            name: 'query',
-            url: '/query',
-            templateUrl: '/home/getview?viewName=query',
-            controller: 'queryCtrl'
-        };
+    var queryState = {
+        name: 'query',
+        url: '/query',
+        templateUrl: '/home/getview?viewName=query',
+        controller: 'queryCtrl'
+    };
 
-        var settingState = {
-            name: 'setting',
-            url: '/setting',
-            templateUrl: '/home/getview?viewName=setting',
-            controller: 'settingCtrl'
-        };
+    var settingState = {
+        name: 'setting',
+        url: '/setting',
+        templateUrl: '/home/getview?viewName=setting',
+        controller: 'settingCtrl'
+    };
 
-        $stateProvider.state(dashState);
-        $stateProvider.state(queryState);
-        $stateProvider.state(settingState);
+    $stateProvider.state(dashState);
+    $stateProvider.state(queryState);
+    $stateProvider.state(settingState);
 
-        $urlRouterProvider.otherwise("/dash");
-    }
+    $urlRouterProvider.otherwise("/dash");
+}
 );
